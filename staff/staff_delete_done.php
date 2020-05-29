e1654007254554defcc8c699a5198eca937381c6
大関千尋 <ozeki@freedomland.jp>
2020-05-29 11:13:49 +0900

    新規作成

---
+++ staff/staff_delete_done.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ろくまる農園</title>
+</head>
+<body>
+<?php
+try {
+    $staff_code = $_POST['code'];
+
+    $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn,$user,$password);
+    // これだとエラーになる
+    // $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'DELETE FROM mst_staff WHERE code=?';
+    $stmt = $dbh -> prepare($sql);
+    $data[] = $staff_code;
+    $stmt->execute($data);
+
+    $dbh = null;
+} catch (Exception $e) {
+    print 'ただいま障害により大変ご迷惑をおかけしております。';
+    exit();
+}
+
+?>
+
+削除しました。<br />
+<br />
+
+<a href="staff_list.php">戻る</a>
+
+</body>
+</html>
