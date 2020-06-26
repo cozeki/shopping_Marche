1f150e54fb5062bd3e38021785f7b98e1745d697
大関千尋 <ozeki@freedomland.jp>
2020-06-26 12:49:45 +0900

    商品削除機能追加

--- php_study/shop/kazu_change.php
+++ php_study/shop/kazu_change.php
@@ -11,6 +11,15 @@ for ($i = 0; $i < $max; $i++) {
     $kazu[] = $post['kazu'.$i];
 }
 
+$cart = $_SESSION['cart'];
+
+for ($i = $max; 0 <= $i; $i--) {
+    if (isset($_POST['sakujo'.$i])) {
+        array_splice($cart, $i, 1);
+        array_splice($kazu, $i, 1);
+    }
+}
+$_SESSION['cart'] = $cart;
 $_SESSION['kazu'] = $kazu;
 
 header('Location:shop_cartlook.php');
